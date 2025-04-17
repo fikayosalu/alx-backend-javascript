410f1ab9967dd83aaa0331f3c73f8ccd8d8f0b84
fikayosalu <salufikayo@gmail.com>
2025-04-17 21:18:20 +0100

    Completed task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(str) {
+    this._name = str;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
