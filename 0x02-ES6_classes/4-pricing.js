343ae83a30f1ce6dd1d0735a6de2c7f76a9d530d
fikayosalu <salufikayo@gmail.com>
2025-04-17 21:48:34 +0100

    Completed task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (!(currency instanceof Currency)) {
+      throw new Error('currency must be an instance of Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
