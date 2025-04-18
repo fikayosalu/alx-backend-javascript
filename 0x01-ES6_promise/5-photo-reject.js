79e77c857ad9c3d3199a0afb51c4fe4511ec083c
fikayosalu <salufikayo@gmail.com>
2025-04-18 21:54:53 +0100

    Completed task 5

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,12 @@
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    if (!filename) {
+      resolve({
+        status: 200,
+        body: filename,
+      });
+    } else {
+      reject(new Error(`${filename} cannot be uploaded`));
+    }
+  });
+}
