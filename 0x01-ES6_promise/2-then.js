7ad70070679a95ed22cd2da0d44c7266f4ea4c4a
fikayosalu <salufikayo@gmail.com>
2025-04-18 20:59:37 +0100

    Completed task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  Promise((resolve, reject) => {
-    resolve({
-      status: 200,
-      body: 'Success',
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (Error()))
+    .finally(() => {
+      console.log('Got a response from the API');
     });
-    reject(new Error());
-  });
-
-  promise.then(() => {
-    console.log('Got a response from the API');
-  });
 }
