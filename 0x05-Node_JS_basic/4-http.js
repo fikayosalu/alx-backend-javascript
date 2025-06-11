b203b13696eccd12b20bedd8f453fe52b7204aa7
fikayosalu <salufikayo@gmail.com>
2025-06-11 05:17:57 +0100

    Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,14 @@
-const http = require('http');
+const { createServer } = require('http');
 
-const app = http.createServer((req, res) => {
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello ALX!');
 });
-
-app.listen(1245);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
